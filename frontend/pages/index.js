382909f9b3f9d876a111df1152ca6414ab8001df
ssgifer <sasi.gifer@gmail.com>
2021-04-19 02:48:34 +0700

    commit

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,10 +3,6 @@ import Layout from '../components/layout'
 import Navbar from '../components/navbar'
 import styles from '../styles/Home.module.css'
 
-export function getServerSideProps({ req, res }) {
-  // console.log("token from cookie: ",cookie.get("token")) 
-  // console.log('req: ', req.headers)
-  
 export default function Home({ token }) {
  
   return (
@@ -19,7 +15,13 @@ export default function Home({ token }) {
         <h1>Puppy Shop</h1>
         <img src ="https://cms.kapook.com/uploads/tag/34/ID_33400_57eded5c4a93a.png" />
     </div>
+    
 </Layout>
   )
 }
+
+export function getServerSideProps({ req, res }) {
+  // console.log("token from cookie: ",cookie.get("token")) 
+  // console.log('req: ', req.headers)
+  return { props: { token: req.cookies.token || "" } };
 }
